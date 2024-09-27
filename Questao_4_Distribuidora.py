6fae0a4c5bb7245300d88efda9bc22b384f134a9
Jeferson Nascimento <jefersonjs42@gmail.com>
2024-09-26 23:19:20 -0300

    Desafio target

--- Questao_4_Distribuidora.py
+++ Questao_4_Distribuidora.py
@@ -1,14 +1,14 @@
 def calcular_percentuais(faturamentos):
-    # Calcula o faturamento total
+    # Calcular o faturamento total
     total = sum(faturamentos.values())
     
-    # Calcula e imprime o percentual de cada estado
+    # Calcular e imprime o percentual de cada estado
     for estado, valor in faturamentos.items():
         percentual = (valor / total) * 100
         print(f"{estado}: R${valor:.2f} - {percentual:.2f}% de representação")
 
 def main():
-    # Dicionário com os estados e seus respectivos faturamentos
+    # Estados e seus faturamentos
     faturamentos = {
         'SP': 67836.43,
         'RJ': 36678.66,
@@ -17,7 +17,7 @@ def main():
         'Outros': 19849.53
     }
     
-    # Chama a função para calcular e exibir os percentuais
+    # Imprimir os percentuais
     calcular_percentuais(faturamentos)
 
 if __name__ == "__main__":
